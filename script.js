cf2d3bfd7a920f0c7a376ddc9eccae7142d62936
Raerek1 <42894808+Raerek1@users.noreply.github.com>
2018-09-01 19:53:53 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+menu.onclick = function MyFunction() {
+	var x = document.getElementById('mytopnav');
+
+	if (x.className === "topnav") {
+		x.className +=" responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
